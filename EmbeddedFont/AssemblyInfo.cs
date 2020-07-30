3cee1d7400d21b9d90240c0ea3ad3a36c86db53e
Rituraj Pandey <rituraj.pandey786@mail.com>
2020-07-30 07:57:26 +0530

    Fist Commit

---
+++ EmbeddedFont/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("materialdesign.ttf", Alias = "IconFont")]
+[assembly: ExportFont("Pacifico-Regular.ttf", Alias = "PacificoRegular")]
